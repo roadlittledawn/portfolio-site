15119a658c67448a86650e3bf6000af86fb818fe
Clinton <clinton.langosch@gmail.com>
2021-03-01 15:25:47 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  siteMetadata: {
+    title: "Clinton Langosch Portfolio Site",
+  },
+  plugins: [
+    "gatsby-plugin-emotion",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "263906713",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
