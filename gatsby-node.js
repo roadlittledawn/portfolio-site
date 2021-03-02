5d573966f5743958141587f67952ca400748a180
Clinton <clinton.langosch@gmail.com>
2021-03-01 21:01:53 -0600

    chore: add rest of career data to graphql

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,31 @@
-const { skills, projects } = require("./src/data/careerData.json");
+const {
+  skills,
+  projects,
+  basics,
+  work,
+} = require("./src/data/careerData.json");
 
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions;
 
   createTypes(`
+    type Basics implements Node {
+      name: String!
+      label: String
+      email: String
+      summary: String
+      profiles: [Profile]
+      headline: String
+      yearsOfExperience: Int
+      username: String
+      locationAsString: String
+      picture: String
+    }
+    type Profile {
+      network: String
+      username: String
+      url: String
+    }
     type Skills implements Node {
       name: String!
       level: String
@@ -20,11 +42,53 @@ exports.createSchemaCustomization = ({ actions }) => {
       githubUrl: String
       repositoryUrl: String
     }
+    type Work implements Node {
+      name: String!
+      position: String
+      startDate: String
+      endDate: String
+      summary: String
+      highlights: [String]
+      isCurrentRole: Boolean
+      start: DateObject
+      end: DateObject
+      company: String
+    }
+    type DateObject {
+      year: Int
+      month: Int
+    }
   `);
 };
 
 exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
   const { createNode } = actions;
+
+  const id = createNodeId("basics");
+  const data = ({
+    name,
+    label,
+    email,
+    summary,
+    profiles,
+    headline,
+    yearsOfExperience,
+    username,
+    locationAsString,
+    picture,
+  } = basics);
+
+  createNode({
+    ...data,
+    id,
+    parent: null,
+    children: [],
+    internal: {
+      type: "Basics",
+      contentDigest: createContentDigest(data),
+    },
+  });
+
   skills.map(({ name, level, rating, yearsOfExperience }, idx) => {
     const id = createNodeId(`${idx}-${name}`);
     const data = {
@@ -76,6 +140,47 @@ exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
         children: [],
         internal: {
           type: "Projects",
+          contentDigest: createContentDigest(data),
+        },
+      });
+    }
+  );
+  work.map(
+    (
+      {
+        name,
+        position,
+        startDate,
+        endDate,
+        summary,
+        highlights,
+        isCurrentRole,
+        start,
+        end,
+        company,
+      },
+      idx
+    ) => {
+      const id = createNodeId(`${idx}-${name}`);
+      const data = {
+        name,
+        position,
+        startDate,
+        endDate,
+        summary,
+        highlights,
+        isCurrentRole,
+        start,
+        end,
+        company,
+      };
+      createNode({
+        ...data,
+        id,
+        parent: null,
+        children: [],
+        internal: {
+          type: "Work",
           contentDigest: createContentDigest(data),
         },
       });
