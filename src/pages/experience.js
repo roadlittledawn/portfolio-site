5c31b2ff5e31553d9eb528dff46c4c9f3daae67d
Clinton <clinton.langosch@gmail.com>
2021-09-12 09:33:06 -0700

    feat(experience): add timeline look

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -16,31 +16,72 @@ const ExperiencePage = ({ data }) => {
     <>
       <MainLayout>
         <h1>Experience</h1>
-        {gigs
-          .sort((a) => (a.isCurrentRole ? -1 : 1))
-          .map((gig, index) => (
-            <section
-              key={`gig-${index}`}
-              css={css`
-                margin: 3rem 0;
-              `}
-            >
-              <h2>{gig.position}</h2>
-              <div>{gig.name}</div>
-              <div>
-                {`${getNameOfMonth(gig.start.month)} ${gig.start.year}`} {`to`}{" "}
-                {gig.isCurrentRole
-                  ? "Present"
-                  : `${getNameOfMonth(gig.start.month)} ${gig.end.year}`}
+        <section
+          css={css`
+            margin: 3em 0;
+          `}
+        >
+          {gigs
+            .sort((a) => (a.isCurrentRole ? -1 : 1))
+            .map((gig, index) => (
+              <div
+                key={`gig-${index}`}
+                css={css`
+                  position: relative;
+                  padding: 0 30px 60px 30px;
+
+                  :last-child {
+                    padding-bottom: 0;
+                    margin-top: -1px;
+                  }
+
+                  :not(:last-child):before {
+                    content: "";
+                    position: absolute;
+                    left: 0;
+                    width: 1px;
+                    height: 100%;
+                    background-color: var(--color-teal-400);
+                  }
+                `}
+              >
+                <div
+                  css={css`
+                    font-size: 0.8em;
+                    padding: 0.5em;
+                    position: relative;
+                    top: -17px;
+                    display: inline-block;
+                    margin-bottom: 1em;
+                    border: 1px solid var(--color-teal-400);
+
+                    :before {
+                      content: "";
+                      width: 30px;
+                      position: absolute;
+                      height: 1px;
+                      transform: translate(-37px, 10px);
+                      background-color: var(--color-teal-400);
+                    }
+                  `}
+                >
+                  {`${getNameOfMonth(gig.start.month)} ${gig.start.year}`}{" "}
+                  {`to`}{" "}
+                  {gig.isCurrentRole
+                    ? "Present"
+                    : `${getNameOfMonth(gig.start.month)} ${gig.end.year}`}
+                </div>
+                <h2>{gig.position}</h2>
+                <div>{gig.name}</div>
+                <p>{gig.summary}</p>
+                <ul>
+                  {gig.highlights.map((highlight, index) => (
+                    <li key={`highlight-${index}`}>{highlight}</li>
+                  ))}
+                </ul>
               </div>
-              <p>{gig.summary}</p>
-              <ul>
-                {gig.highlights.map((highlight, index) => (
-                  <li key={`highlight-${index}`}>{highlight}</li>
-                ))}
-              </ul>
-            </section>
-          ))}
+            ))}
+        </section>
       </MainLayout>
     </>
   );
