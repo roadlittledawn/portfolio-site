4a300bc7cbb44e8aef7805c7c44e8f4328051f4a
Clinton <clinton.langosch@gmail.com>
2022-03-02 10:38:22 -0800

    feat: add github workflow to auto generate resume PDF

--- scripts/generateResumePdf.js
+++ scripts/generateResumePdf.js
@@ -4,19 +4,14 @@ const path = require("path");
 
 // https://www.npmjs.com/package/html-pdf-node
 
-let options = { format: "A4" };
-// Example of options with args //
-// let options = { format: 'A4', args: ['--no-sandbox', '--disable-setuid-sandbox'] };
+const options = { format: "A4", printBackground: true };
 
-const file = {
-  content: fs.readFileSync(path.join(process.cwd(), "src/files/resume.html")),
-};
-// or
-// const file = { url: "https://gitconnected.com/roadlittledawn/resume" };
+const file = { url: "https://clintonlangosch.com/resume/?embed=true" };
+
 htmlToPdf.generatePdf(file, options).then((pdfBuffer) => {
   console.log("PDF Buffer:-", pdfBuffer);
   fs.writeFileSync(
-    path.join(process.cwd(), "src/files/resume-local.pdf"),
+    path.join(process.cwd(), "src/files/resume-eng.pdf"),
     pdfBuffer
   );
 });
