83b189716a5521556688784dc0043a797d1187a7
Clinton <clinton.langosch@gmail.com>
2022-08-16 16:50:52 -0700

    feat: add years of service for each gig in timeline

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -5,6 +5,8 @@ import { graphql } from "gatsby";
 import MainLayout from "../layouts/MainLayout";
 import PageTitle from "../components/PageTitle";
 import SEO from "../components/SEO";
+import { calculateTimeBetweenDates } from "../utils/time";
+import { pluralize } from "../utils/pluralize";
 
 const ExperiencePage = ({ data }) => {
   const {
@@ -24,64 +26,82 @@ const ExperiencePage = ({ data }) => {
         >
           {gigs
             .sort((a) => (a.isCurrentRole ? -1 : 0))
-            .map((gig, index) => (
-              <div
-                key={`gig-${index}`}
-                css={css`
-                  position: relative;
-                  padding: 0 30px 60px 30px;
-
-                  :last-child {
-                    padding-bottom: 0;
-                    margin-top: -1px;
-                  }
-
-                  :not(:last-child):before {
-                    content: "";
-                    position: absolute;
-                    left: 0;
-                    width: 1px;
-                    height: 100%;
-                    background-color: var(--color-teal-400);
-                  }
-                `}
-              >
+            .map((gig, index) => {
+              const yearsOfService = calculateTimeBetweenDates({
+                unit: "years",
+                start: [gig.start.year, gig.start.month],
+                end: gig.end.year
+                  ? [gig.end.year, gig.end.month]
+                  : [new Date().getFullYear(), new Date().getMonth()],
+                returnFloat: true,
+              });
+              return (
                 <div
+                  key={`gig-${index}`}
                   css={css`
-                    font-size: 0.8em;
-                    padding: 0.5em;
                     position: relative;
-                    top: -17px;
-                    display: inline-block;
-                    margin-bottom: 1em;
-                    border: 1px solid var(--color-teal-400);
+                    padding: 0 30px 60px 30px;
 
-                    :before {
+                    :last-child {
+                      padding-bottom: 0;
+                      margin-top: -1px;
+                    }
+
+                    :not(:last-child):before {
                       content: "";
-                      width: 30px;
                       position: absolute;
-                      height: 1px;
-                      transform: translate(-37px, 10px);
+                      left: 0;
+                      width: 1px;
+                      height: 100%;
                       background-color: var(--color-teal-400);
                     }
                   `}
                 >
-                  {`${getNameOfMonth(gig.start.month)} ${gig.start.year}`}{" "}
-                  {`to`}{" "}
-                  {gig.isCurrentRole
-                    ? "Present"
-                    : `${getNameOfMonth(gig.end.month)} ${gig.end.year}`}
+                  <div
+                    css={css`
+                      font-size: 0.8em;
+                      padding: 0.5em 1em;
+                      position: relative;
+                      top: -17px;
+                      display: inline-block;
+                      margin-bottom: 1em;
+                      border: 1px solid var(--color-teal-400);
+
+                      :before {
+                        content: "";
+                        width: 30px;
+                        position: absolute;
+                        height: 1px;
+                        transform: translate(-43px, 10px);
+                        background-color: var(--color-teal-400);
+                      }
+                    `}
+                  >
+                    {`${gig.start.year}`} {`to`}{" "}
+                    {gig.isCurrentRole ? "Present" : `${gig.end.year}`}
+                  </div>
+                  <div
+                    css={css`
+                      display: inline-block;
+                      margin: 0 0 1em 1em;
+                      position: relative;
+                      top: -17px;
+                      font-size: 0.85em;
+                    `}
+                  >
+                    {pluralize(yearsOfService, "year")}
+                  </div>
+                  <h2>{gig.position}</h2>
+                  <div>{gig.name}</div>
+                  <p>{gig.summary}</p>
+                  <ul>
+                    {gig.highlights.map((highlight, index) => (
+                      <li key={`highlight-${index}`}>{highlight}</li>
+                    ))}
+                  </ul>
                 </div>
-                <h2>{gig.position}</h2>
-                <div>{gig.name}</div>
-                <p>{gig.summary}</p>
-                <ul>
-                  {gig.highlights.map((highlight, index) => (
-                    <li key={`highlight-${index}`}>{highlight}</li>
-                  ))}
-                </ul>
-              </div>
-            ))}
+              );
+            })}
         </section>
       </MainLayout>
     </>
@@ -91,13 +111,6 @@ const ExperiencePage = ({ data }) => {
 ExperiencePage.propTypes = {
   data: PropTypes.object,
 };
-
-const getNameOfMonth = (monthNumber) =>
-  new Date(2021, monthNumber - 1, 10).toLocaleString("default", {
-    month: "long",
-  });
-
-const convertToUnixTime = (dateStamp) => new Date(dateStamp).getTime();
 
 export const pageQuery = graphql`
   query {
--- src/utils/time.js
+++ src/utils/time.js
@@ -1,10 +1,45 @@
+import moment from "moment";
+
 /**
  * Returns human friendly name of month given the month's calendar number
  *
  * @param {number} monthNumber Calendar number of the month 1-12
  * @returns {string} Human friendly full name of month
  */
-export const getNameOfMonth = (monthNumber) =>
+const getNameOfMonth = (monthNumber) =>
   new Date(2021, monthNumber - 1, 10).toLocaleString("default", {
     month: "long",
   });
+
+/**
+ *
+ * @param {number} dateStamp
+ * @returns Unix timestamp
+ */
+
+const convertToUnixTime = (dateStamp) => new Date(dateStamp).getTime();
+
+/**
+ * @param {string} unit What unit of time to calculate. only 'years' currently supported
+ * @param {array} start Array of date stamp. Example: [2022, 4, 23]
+ * @param {array} end Array of date stamp. Example: [2022, 5, 23]
+ */
+
+const calculateTimeBetweenDates = ({
+  unit,
+  start,
+  end,
+  returnFloat = false,
+  roundUp = true,
+}) => {
+  var a = moment(end);
+  var b = moment(start);
+  // var b = moment([2020, 8, 1]);
+  // var a = moment([2022, 8, 1]);
+  console.log(a.diff(b, unit, returnFloat));
+  return roundUp
+    ? Math.ceil(a.diff(b, unit, returnFloat))
+    : a.diff(b, unit, returnFloat);
+};
+
+export { getNameOfMonth, convertToUnixTime, calculateTimeBetweenDates };
