9a9076200d60b1cb598a4c92070d3b8023012171
Clinton <clinton.langosch@gmail.com>
2023-08-24 08:44:54 -0700

    wip: create component

---
+++ src/components/ScrabbleTiles.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import React from 'react';
+import { css } from '@emotion/react';
+
+const ScrabbleTiles = ({word}) => {
+  return (
+    
+  )
+}
+
+ScrabbleTiles.propTypes = {
+  word: PropTypes.string.isRequired,
+};
+
+export default ScrabbleTiles;
