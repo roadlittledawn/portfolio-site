eac97e452940613d9ce255a3e4da4aca0c89a519
roadlittledawn <clinton.langosch@gmail.com>
2026-03-24 18:46:13 -0700

    refactor: change loading text

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -7,11 +7,22 @@ export default function Loading() {
         fill="none"
         viewBox="0 0 24 24"
       >
-        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
-        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z" />
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        />
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+        />
       </svg>
       <span className="text-text-secondary">
-        Waking up the free serverless function from its cold, dead start
+        Loading data (slowly but at freely 🤑)
       </span>
     </div>
   );
